924442f9086d57c61da6c1218bbcbd93fede6137
Raheman Ali <49222155+Raheman-08@users.noreply.github.com>
2024-03-06 09:18:36 +0530

    initial commit

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 // config/db.js
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/Carefit', {
+mongoose.connect('mongodb+srv://raheman:raheman@cluster0.6yr8bq0.mongodb.net/carefit', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
